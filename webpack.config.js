d265a4c47f23090a3b1d632959b74d37dd02daae
Matt Cann <Random_mac@hotmail.com>
2024-08-08 13:18:48 +0100

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new Dotenv()
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+    },
+};
